35021b45cc8bd5632c1195d2dc8f960a7e8eef56
Boris Rasin <boris@pointx.org>
2024-03-25 22:21:42 +0200

    fix: add header java_api.h to support deprecated generated headers 1.0

--- source/scapix/java_api.h
+++ source/scapix/java_api.h
@@ -1,11 +1,13 @@
 /*
-	scapix/jni/java_api/init.h
+	scapix/java_api.h
 
 	Copyright (c) 2019-2024 Boris Rasin (boris@scapix.com)
+
+	Deprecated: for compatibility with scapix_java 1.0 generated headers.
 */
 
-#ifndef SCAPIX_JNI_JAVA_API_INIT_H
-#define SCAPIX_JNI_JAVA_API_INIT_H
+#ifndef SCAPIX_JAVA_API_H
+#define SCAPIX_JAVA_API_H
 
 #include <scapix/jni/object_base.h>
 #include <scapix/jni/ref.h>
@@ -14,6 +16,11 @@
 
 namespace scapix::java_api {
 
+#define SCAPIX_META_STRING(x) x
+using jni::object_base;
+using jni::ref;
+namespace link::java { using jni::array; }
+
 // workaround for conflict between namespaces "sun" and "com::sun"
 
 namespace sun {}
@@ -21,4 +28,4 @@ namespace com::sun { namespace sun = ::scapix::java_api::sun; }
 
 } // namespace scapix::java_api
 
-#endif // SCAPIX_JNI_JAVA_API_INIT_H
+#endif // SCAPIX_JAVA_API_H
